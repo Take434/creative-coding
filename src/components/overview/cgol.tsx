987a0c4c1d3ef008fca7f50e14ca621e5e9ad902
Take434 <timh4376@gmail.com>
2026-04-18 00:09:18 +0200

    wip palette generation

--- src/components/overview/cgol.tsx
+++ src/components/overview/cgol.tsx
@@ -2,7 +2,38 @@ import { useTheme } from "@/lib/states";
 import { useFrame } from "@react-three/fiber";
 import { useEffect, useMemo, useRef } from "react";
 import { Vector2, Vector3 } from "three";
-import { oklch, rgb, type Oklch } from "culori";
+import { oklch, random, rgb, type Oklch } from "culori";
+
+const frag2 = `
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+uniform vec2 u_resolution;
+uniform float u_time;
+uniform vec3 u_palette[8];
+
+const int u_steps = 8;
+
+vec3 posterize(vec3 c) {
+    c = c * float(u_steps);
+    c = floor(c);
+
+    vec3 ret = c;
+    for(int i = 0; i < u_steps; i++) {
+        ret = ret * min(abs(float(i) - c.x), 1.) + u_palette[i] * abs(min(abs(float(i) - c.x), 1.) - 1.);
+    }
+    
+	return ret;
+}
+
+void main(){
+	vec3 color = (gl_FragCoord.x / u_resolution.x) * vec3(1.0, 1.0, 1.0);
+    color = posterize(color);
+
+    gl_FragColor = vec4(color,1.0);
+}
+`;
 
 const frag = `
 #ifdef GL_ES
@@ -53,50 +84,66 @@ void main() {
 }
 `;
 
+const randomRGB = () => {
+  const r = random();
+  return new Vector3(r.r, r.g, r.b);
+};
+
 export function ConwaysGOL() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const shaderRef = useRef<any>(null!);
   const theme = useTheme((state) => state.isDark);
 
   useEffect(() => {
-    const cssColorBG = getComputedStyle(
-      document.querySelector("#root")!,
-    ).getPropertyValue("--background");
+    // const cssColorBG = getComputedStyle(
+    //   document.querySelector("#root")!,
+    // ).getPropertyValue("--background");
 
-    const cssColorACC = getComputedStyle(
-      document.querySelector("#root")!,
-    ).getPropertyValue("--foreground");
+    // const cssColorACC = getComputedStyle(
+    //   document.querySelector("#root")!,
+    // ).getPropertyValue("--foreground");
 
-    const tempCssColorBG = cssColorBG
-      .replace("oklch(", "")
-      .replace(")", "")
-      .split(" ")
-      .map((x) => Number.parseFloat(x)) as [number, number, number];
+    // const tempCssColorBG = cssColorBG
+    //   .replace("oklch(", "")
+    //   .replace(")", "")
+    //   .split(" ")
+    //   .map((x) => Number.parseFloat(x)) as [number, number, number];
 
-    const tempCssColorACC = cssColorACC
-      .replace("oklch(", "")
-      .replace(")", "")
-      .split(" ")
-      .map((x) => Number.parseFloat(x)) as [number, number, number];
+    // const tempCssColorACC = cssColorACC
+    //   .replace("oklch(", "")
+    //   .replace(")", "")
+    //   .split(" ")
+    //   .map((x) => Number.parseFloat(x)) as [number, number, number];
 
-    const oklchBG: Oklch = {
-      mode: "oklch",
-      l: tempCssColorBG[0],
-      c: tempCssColorBG[1],
-      h: tempCssColorBG[2],
-    };
-    const bg = rgb(oklch(oklchBG));
+    // const oklchBG: Oklch = {
+    //   mode: "oklch",
+    //   l: tempCssColorBG[0],
+    //   c: tempCssColorBG[1],
+    //   h: tempCssColorBG[2],
+    // };
+    // const bg = rgb(oklch(oklchBG));
 
-    const oklchACC: Oklch = {
-      mode: "oklch",
-      l: tempCssColorACC[0],
-      c: tempCssColorACC[1],
-      h: tempCssColorACC[2],
-    };
-    const acc = rgb(oklch(oklchACC));
+    // const oklchACC: Oklch = {
+    //   mode: "oklch",
+    //   l: tempCssColorACC[0],
+    //   c: tempCssColorACC[1],
+    //   h: tempCssColorACC[2],
+    // };
+    // const acc = rgb(oklch(oklchACC));
 
-    shaderRef.current.uniforms.u_background_color.value = bg;
-    shaderRef.current.uniforms.u_accent_color.value = acc;
+    // shaderRef.current.uniforms.u_background_color.value = bg;
+    // shaderRef.current.uniforms.u_accent_color.value = acc;
+
+    shaderRef.current.uniforms.u_palette.value = [
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+      randomRGB(),
+    ];
   }, [theme]);
 
   const uniforms = useMemo(
@@ -106,6 +153,18 @@ export function ConwaysGOL() {
       u_mouse: { value: new Vector2() },
       u_background_color: { value: new Vector3() },
       u_accent_color: { value: new Vector3() },
+      u_palette: {
+        value: [
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+          new Vector3(),
+        ],
+      },
     }),
     [],
   );
@@ -125,7 +184,7 @@ export function ConwaysGOL() {
       <shaderMaterial
         ref={shaderRef}
         vertexShader={vertex}
-        fragmentShader={frag}
+        fragmentShader={frag2}
         uniforms={uniforms}
       />
     </mesh>
