29f70c72d2e059cdde89aa19d18e595df756a2c1
Take434 <timh4376@gmail.com>
2026-04-27 11:54:40 +0200

    added monoton pallette picker

--- src/projects/farbfleck/farbfleck.tsx
+++ src/projects/farbfleck/farbfleck.tsx
@@ -1,8 +1,17 @@
 import { farbfleckFragShader } from "@/projects/farbfleck/fragmentshader";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { random } from "culori";
-import { useMemo, useRef } from "react";
+import { converter, type Oklch, random, type Rgb } from "culori";
+import { useMemo, useRef, useState } from "react";
 import { Vector2, Vector3 } from "three";
+import {
+  ColorPicker,
+  ColorPickerEyeDropper,
+  ColorPickerFormat,
+  ColorPickerHue,
+  ColorPickerOutput,
+  ColorPickerSelection,
+} from "@/components/kibo-ui/color-picker";
+import { Button } from "@/components/ui/button";
 
 const vertex = `
 void main() {
@@ -10,23 +19,115 @@ void main() {
 }
 `;
 
-const randColor = () => {
-  const r = random();
-  return new Vector3(r.r, r.g, r.b);
-};
+type Colors = [Rgb, Rgb, Rgb, Rgb, Rgb, Rgb, Rgb, Rgb];
 
 export function Farbfleck() {
+  const rands = (): Colors => {
+    return [
+      random(),
+      random(),
+      random(),
+      random(),
+      random(),
+      random(),
+      random(),
+      random(),
+    ];
+  };
+
+  const [colors, setColors] = useState<Colors>(rands);
+
+  const fillRandom = () => {
+    setColors(rands());
+  };
+
+  const generatePalette = (primary: Rgb) => {
+    const lch = converter("oklch")(primary);
+    const monoton = generateMonotonSwatches(lch, 8);
+
+    const conv = converter("rgb");
+    setColors(monoton.map((x) => conv(x)) as Colors);
+  };
+
+  const generateMonotonSwatches = (color: Oklch, count: number): Oklch[] => {
+    //to keep it interesting, we limit the values of the lightness between 0.05 and 0.95 thats why we only divide 90 not 100
+    const stepSize = 0.9 / count;
+    const mediumCol: Oklch = { ...color, l: 0.5 };
+    const darker: Oklch[] = [];
+    const lighter: Oklch[] = [];
+
+    for (let i = 0; i < count / 2; i++) {
+      const prev = i > 0 ? lighter[i - 1] : mediumCol;
+      lighter[i] = { ...prev, l: prev.l + stepSize };
+    }
+
+    for (let i = 0; i < count / 2; i++) {
+      const prev = i > 0 ? darker[i - 1] : mediumCol;
+      darker[i] = { ...prev, l: prev.l - stepSize };
+    }
+
+    darker.reverse();
+    let steps = [...darker, mediumCol, ...lighter];
+    steps.reverse();
+    steps = steps.slice(0, count);
+    return steps as Oklch[];
+  };
+
   return (
-    <div className="h-full">
+    <div className="h-full flex">
       <Canvas style={{ height: "80vh", aspectRatio: 1 / 1, width: "auto" }}>
         <ambientLight intensity={1} />
-        <Shader />
+        <Shader colors={colors} />
       </Canvas>
+      <div className="h-[80vh] ml-2">
+        {colors.map((x, i) => (
+          <div
+            className="h-[10vh] aspect-square"
+            style={{
+              backgroundColor: `rgb(${x.r * 255} ${x.g * 255} ${x.b * 255})`,
+            }}
+            key={i}
+          ></div>
+        ))}
+      </div>
+      <div className="ml-6 flex flex-col">
+        <h2 className="text-2xl font-bold">Custom Monoton Palette</h2>
+        <div>
+          <ColorPicker
+            className="max-w-sm rounded-md border bg-background p-4 shadow-sm mt-2"
+            onChange={(x) =>
+              generatePalette({
+                r: x[0] / 255.0,
+                g: x[1] / 255.0,
+                b: x[2] / 255.0,
+                mode: "rgb",
+              } as Rgb)
+            }
+          >
+            <ColorPickerSelection className="aspect-square" />
+            <div className="flex items-center gap-4">
+              <ColorPickerEyeDropper />
+              <div className="grid w-full gap-1">
+                <ColorPickerHue />
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <ColorPickerOutput />
+              <ColorPickerFormat />
+            </div>
+          </ColorPicker>
+        </div>
+        <hr className="my-5" />
+        <h2 className="text-2xl font-bold">Generate Random Colors</h2>
+        <Button className="mt-2" onClick={() => fillRandom()}>
+          Generate
+        </Button>
+      </div>
     </div>
   );
 }
 
-function Shader() {
+function Shader({ colors }: { colors: Colors }) {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const shaderRef = useRef<any>(null!);
 
@@ -35,19 +136,10 @@ function Shader() {
       u_time: { value: 1.0 },
       u_resolution: { value: new Vector2() },
       u_colors: {
-        value: [
-          randColor(),
-          randColor(),
-          randColor(),
-          randColor(),
-          randColor(),
-          randColor(),
-          randColor(),
-          randColor(),
-        ],
+        value: colors.map((x) => new Vector3(x.r, x.g, x.b)),
       },
     }),
-    [],
+    [colors],
   );
 
   useFrame((state) => {
